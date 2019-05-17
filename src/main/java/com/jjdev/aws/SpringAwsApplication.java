93ab0f3ba81a77c1416ab0245ca205776440f7b5
jgilson <jgilson@leucotron.com.br>
2019-05-17 15:43:29 -0300

    Add environment configurations

--- src/main/java/com/jjdev/aws/SpringAwsApplication.java
+++ src/main/java/com/jjdev/aws/SpringAwsApplication.java
@@ -2,9 +2,16 @@ package com.jjdev.aws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringAwsApplication {
+public class SpringAwsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringAwsApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAwsApplication.class, args);
